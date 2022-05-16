f290f4b2f00cc3460e45b525eb822fe6824d986d
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2022-05-16 15:01:57 +0200

    Implement Show/Hide extension

--- text-editor-show-hide/src/extension.ts
+++ text-editor-show-hide/src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "text-editor-show-hide" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('text-editor-show-hide.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Text Editor Show/Hide!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('text-editor-show-hide.show', () => {
+			vscode.window.activeTextEditor?.show(vscode.ViewColumn.Beside);
+		}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('text-editor-show-hide.showOne', () => {
+			vscode.window.activeTextEditor?.show(vscode.ViewColumn.One);
+		}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('text-editor-show-hide.showTwo', () => {
+			vscode.window.activeTextEditor?.show(vscode.ViewColumn.Two);
+		}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('text-editor-show-hide.showThree', () => {
+			vscode.window.activeTextEditor?.show(vscode.ViewColumn.Three);
+		}));
+	context.subscriptions.push(
+		vscode.commands.registerCommand('text-editor-show-hide.hide', () => {
+			vscode.window.activeTextEditor?.hide();
+		}));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
